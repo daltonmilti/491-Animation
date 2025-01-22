eeeb46261d08dafa6951a0113834c9dd38daf1e1
daltonmilti <147172231+daltonmilti@users.noreply.github.com>
2025-01-21 18:55:19 -0800

    added animator

--- chicken.js
+++ chicken.js
@@ -1,13 +1,18 @@
 class Chicken {
-    constructor() {
+    constructor(game) {
+        this.game = game;
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./chick_24x24.png"), 0, 0, 24, 24, 7, 0.2);
 
+        this.x = 0;
+        this.y = 0;
+        this.speed = 100;
     };
 
-    udpate() {
-
+    update() {
+        this.x += this.speed * this.game.clockTick;
     };
 
     draw(ctx) {
-        ctx.drawImage(ASSET_MANAGER.getAsset("./chick_24x24.png"),0,0);
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
     };
 }
--- main.js
+++ main.js
@@ -8,7 +8,7 @@ ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-	gameEngine.addEntity(new Chicken());
+	gameEngine.addEntity(new Chicken(gameEngine));
 
 	gameEngine.init(ctx);
 
